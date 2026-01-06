03f589bf6696bf8ccdd963ad88d880f99be985d3
Papa Ree <79291092+papa-ree@users.noreply.github.com>
2026-01-06 11:25:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Bale\Emperan\Controllers\MediaController;
 use Bale\Emperan\Models\Option;
 
-Route::get('/media/' . Option::whereName('organization_slug')->first() . '{path}', [MediaController::class, 'show'])
+Route::get('/media/' . Option::whereName('organization_slug')->first()->value . '{path}', [MediaController::class, 'show'])
     ->where('path', '.*')
     ->name('media.show');
